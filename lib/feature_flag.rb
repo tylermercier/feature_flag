79b191d86352fa8753562bfb2500c7671ddb2be2
Tyler Mercier <tylermercier@gmail.com>
2013-05-23 18:01:59 -0600

    add rspec file. add skeleton of code

--- lib/feature_flag.rb
+++ lib/feature_flag.rb
@@ -1,5 +1,17 @@
 require "feature_flag/version"
 
 module FeatureFlag
-  # Your code goes here...
+
+  def duck?
+    check_flag "FF_DUCKS"
+  end
+
+  private
+    def check_flag(key)
+      return false if ENV[key].nil? || ENV[key].blank?
+      return false if ENV[key] == false || ENV[key] =~ (/(false|f|no|n|0)$/i)
+      return true if ENV[key] == true || ENV[key] =~ (/(true|t|yes|y|1)$/i)
+
+      raise ArgumentError.new("invalid value for flag: #{key}")
+    end
 end
